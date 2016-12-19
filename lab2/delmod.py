635047d6e5c8b75406fe6fd99490e0eea5fabd79
User <victorvladimirovichkravhcuk@gmail.com>
2016-12-19 19:18:08 +0200

    Update

--- lab2/delmod.py
+++
@@ -1,5 +0,0 @@
-from lab2 import db as DataBase
-
-def delete(table, id):
-	DataBase.delete(DataBase.escapeBySymbol(table, "`"), where = 'WHERE `id` = ' + DataBase.escapeBySymbol(id, "'"))
-	return 1
